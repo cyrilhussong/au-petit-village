948ec661d2e640a67d09e6e2ef8c64549a0c4299
cyrilhussong <cyrilhussong347@gmail.com>
2025-06-13 15:15:22 +0200

    Premier commit du projet Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { ProductPageComponent } from './product-page/product-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 
 const routes: Routes = [
-  { path: '', component: ProductPageComponent },
+  { path: '', component: ProductPageComponent }, // ou HomeComponent si tu en as un
   { path: 'about', component: AboutPageComponent },
   { path: 'contact', component: ContactPageComponent },
+  { path: '**', redirectTo: '' } // redirection pour les routes inconnues
 ];
 
 @NgModule({
